fd8ea2bc0ff77e35c3a3c907f3ff022ceacfffae
Matthew Dalby <matthewdalby@Matthew-MacBook-Primary.local>
2021-09-11 12:11:47 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({id: 1, title: 'sample product'});
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
